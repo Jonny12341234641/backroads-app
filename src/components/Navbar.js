927eb0e92b638629af7079aa1864a7875913a8a1
Kavindu <kavinduuni@gmail.com>
2026-03-21 15:58:01 +0530

    feat: Integrate social links into Navbar component and update data structure

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import logo from "../images/logo.svg";
-import { pageLinks } from "../data";
+import { pageLinks, socialLinks } from "../data";
 
 const Navbar = () => {
   return (
@@ -25,21 +25,15 @@ const Navbar = () => {
         </ul>
 
         <ul className="nav-icons">
-          <li>
-            <a href="https://www.facebook.com" target="_blank" rel="noreferrer" className="nav-icon">
-              <i className="fab fa-facebook"></i>
-            </a>
-          </li>
-          <li>
-            <a href="https://www.twitter.com" target="_blank" rel="noreferrer" className="nav-icon">
-              <i className="fab fa-twitter"></i>
-            </a>
-          </li>
-          <li>
-            <a href="https://www.squarespace.com" target="_blank" rel="noreferrer" className="nav-icon">
-              <i className="fab fa-squarespace"></i>
-            </a>
-          </li>
+            {socialLinks.map((link) => {
+                return(
+                    <li key = {link.id}>
+                        <a href={link.href} target="_blank" rel="noreferrer" className="nav-icon">
+                            <i className={link.icon}></i>
+                        </a>
+                    </li>
+                )
+            })}
         </ul>
       </div>
     </nav>
